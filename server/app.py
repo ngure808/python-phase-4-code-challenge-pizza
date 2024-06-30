08758e3d01e860a02e80fd988a9059f904484e87
ngure808 <mwangingure808@gmail.com>
2024-06-30 21:32:47 +0300

    POST, GET and DELETE functionality

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -23,6 +23,68 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return jsonify([restaurant.serialize() for restaurant in restaurants])
+
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = db.session.get(Restaurant, id)
+    if restaurant:
+        return jsonify(restaurant.serialize(include_pizzas=True))
+    else:
+        return jsonify({'error': 'Restaurant not found'}), 404
+    
+@app.route('/restaurants/<int:id>', methods=['DELETE'])
+def delete_restaurant_by_id(id):
+    restaurant = db.session.get(Restaurant,id)
+
+    if restaurant:
+        db.session.delete(restaurant)
+        db.session.commit()
+        return '', 204 
+    else:
+        return jsonify({'error': 'Restaurant not found'}), 404
+    
+@app.route('/pizzas', methods = ["GET"])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([pizza.serialize() for pizza in pizzas])
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizzas():
+    data = request.json
+
+    # Extract data from request
+    price = data.get('price')
+    pizza_id = data.get('pizza_id')
+    restaurant_id = data.get('restaurant_id')
+    
+    #validates entry of price
+    if not (1 <= price <= 30):
+        return jsonify({'errors': ["validation errors"]}), 400
+
+    # Create new RestaurantPizza instance
+    new_entry = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
+
+    try:
+        db.session.add(new_entry)
+        db.session.commit()
+        return jsonify({
+            'id': new_entry.id,
+            'price': new_entry.price,
+            'pizza_id': new_entry.pizza_id,
+            'restaurant_id': new_entry.restaurant_id,
+            'pizza': new_entry.pizza.serialize() if new_entry.pizza else None,
+            'restaurant': new_entry.restaurant.serialize() if new_entry.restaurant else None
+        }), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 400
+
+
 
 
 if __name__ == "__main__":
